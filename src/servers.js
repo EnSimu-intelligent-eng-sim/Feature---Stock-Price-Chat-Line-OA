a377318e8c400eb052f5a73686563c3258c1e910
Khiw Nitithadachot <kiw.brw@gmail.com>
2025-02-17 12:02:11 +0000

    v1.2.2

--- src/servers.js
+++ src/servers.js
@@ -10,8 +10,18 @@ const lineConfig = {
     channelAccessToken: process.env.ACCESS_TOKEN
 };
 
+// Add debug logging
+console.log('Channel Secret:', process.env.SECRET_TOKEN ? 'Set' : 'Not Set');
+console.log('Channel Access Token:', process.env.ACCESS_TOKEN ? 'Set' : 'Not Set');
+
 const client = new line.Client(lineConfig);
 const app = express();
+
+app.use((req, res, next) => {
+    console.log('Headers:', req.headers);
+    console.log('X-Line-Signature:', req.headers['x-line-signature']);
+    next();
+});
 
 app.post('/webhook', line.middleware(lineConfig), async (req, res) => {
     try {
@@ -24,11 +34,12 @@ app.post('/webhook', line.middleware(lineConfig), async (req, res) => {
 
         res.status(200).send("OK");
     } catch (error) {
-        console.error(error);
+        console.error('Error details:', error);
         res.status(500).end();
     }
 });
 
 app.listen(4000, () => {
     console.log('listening on PORT:4000');
+    console.log('Line Bot Configuration loaded');
 });
